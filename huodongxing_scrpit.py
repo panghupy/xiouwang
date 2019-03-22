bec5cffc0dd78555503b79b05c323ec78dbd73f2
apple <apple@appleiMac.local>
2019-03-22 09:22:10 +0800

    格式化发表内容

--- huodongxing_scrpit.py
+++ huodongxing_scrpit.py
@@ -2,6 +2,7 @@ import requests
 from lxml import etree
 import json
 import time
+from NewsAdd import add
 
 url = 'https://www.huodongxing.com/ranklist'
 headers = {
@@ -9,11 +10,12 @@ headers = {
     'Cookie': 'uid=c3f4fb27-3bd6-4e9e-9d3f-783a47c72155;ASP.NET_SessionId=ddp1frw2zfbxomzd1t4afyvb;Appendhdmodalimg=Appendhdmodalimg;qdcode_2479875859200=628363131252;qdcode_8479347906400=6628363131252;route=1c148779685bf6c8f7fa768a9676bc01;HDX_REGION=%e5%8c%97%e4%ba%ac%2c%e5%8c%97%e4%ba%ac;Hm_lpvt_d89d7d47b4b1b8ff993b37eafb0b49bd=' + str(
         time.time())[:11] + ';_ga=GA1.2.928356410.1552526521;_gid=GA1.2.352944577.1552887423',
 }
-response = requests.get(url, headers=headers).content.decode()
+response = requests.get(url, headers=headers,verify=False).content.decode()
 html = etree.HTML(response)
 with open('test.html', 'w')as f:
     f.write(response)
 popularity_list = html.xpath('.//div[@class="popularity-son-item flex"]')
+print(len(popularity_list))
 for item in popularity_list:
     # 活动标题
     title = item.xpath('.//img[@class="item-logo"]/@alt')[0]
@@ -31,8 +33,11 @@ for item in popularity_list:
     # content_response_html = etree.HTML(content_response)
     # article = content_response_html.xpath('.//div[@class="article"]')[0].xpath('string(.)')
     # print(article)
+    content = '<p>'+"活动标题：" + title + '\n</p><p>' + '活动地点：' + location + '\n</p><p>' + '活动时间：' + activity_time + '\n</p>' + '<p>活动报名链接：<a style="color:red" href=' + activity_link+'>'+activity_link+'</a>'
     print('活动标题：　', title)
     print('活动地点：　', location)
     print('活动时间：　', activity_time)
     print('活动报名链接：　', activity_link)
     print('活动封面图片：　', img_url)
+    # result = add(title=title, content=content)
+    print(result)
